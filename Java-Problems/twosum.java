a543167518db36d28a50f7476707f70ff9e85222
Ghananjani Saini <88573690+ghananjani@users.noreply.github.com>
2024-07-02 23:01:42 +0530

    Update twosum.java

--- Java-Problems/twosum.java
+++ Java-Problems/twosum.java
@@ -1,20 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
-        Map<Integer, Integer> sumMap = new HashMap<>();
-        int [] result = new int[2];
-        
-        for (int i=0; i<nums.length(); i++){
-            
-            if(!sumMap.isEmpty() && sumMap.containsKey(nums[i])){
-                result[0] = sumMap.get(nums[i]);
-                result[1] = i;
-                
-                return result;
-            }
-            else{
-                sumMap.put(target-nums[i],i);
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
             }
         }
+        return null;
     }
 }
