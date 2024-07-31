84493eb90d8f2752e63bc64f6cd08f7e2cb5ab44
Ghananjani Saini <88573690+ghananjani@users.noreply.github.com>
2024-07-31 23:42:03 +0530

    Create linearsearch3.java

---
+++ Java-Practice/linearsearch3.java
@@ -0,0 +1,19 @@
+public class demo {
+    public static void main(String[] args) {
+        int nums[] = {5,7,9,11,13};
+        int target = 11;
+        int result = linearSearch(nums, target);
+        
+        if (result != -1)
+            System.out.println("element found at index: " + result);
+        else 
+        System.out.println("element not found");
+    }
+    public static int linearSearch(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] == target)
+            return i;
+    }
+        return -1;
+    }
+}
