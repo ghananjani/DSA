4303e542e28263fd90a8daf3dba9e941fa596b5d
Ghananjani Saini <88573690+ghananjani@users.noreply.github.com>
2024-08-09 08:46:38 +0530

    Create 1295. Find Numbers with Even Number of Digits.java

---
+++ Java-Problems/1295. Find Numbers with Even Number of Digits.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int findNumbers(int[] nums) {
+        int[] nums = {12,345,2,6,7896};
+//        System.out.println(findNumbers(nums));
+
+        System.out.println(digits2(-345678));
+    }
+    static int findNumbers(int[] nums) {
+        int count = 0;
+        for(int num : nums) {
+            if (even(num)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // function to check whether a number contains even digits or not
+    static boolean even(int num) {
+        int numberOfDigits = digits(num);
+        /*
+        if (numberOfDigits % 2 == 0) {
+            return true;
+        }
+        return false;
+         */
+        return numberOfDigits % 2 == 0;
+    }
+
+    static int digits2(int num) {
+        if (num < 0) {
+            num = num * -1;
+        }
+        return (int)(Math.log10(num)) + 1;
+    }
+    }
